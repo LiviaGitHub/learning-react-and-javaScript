c38f00cc646794db4d1185a5cf3a935db8de5d9d
LiviaGitHub <liviagithub@gmail.com>
2023-09-06 12:31:40 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ function App() {
   return (
     <div>
         <Home />
-      {/* <header>
-        <h1>Welcome to My React App</h1>
-      </header>
-      <main>
-        <section>
-          <h2>About Us</h2>
-          <p>This is a basic React app.</p>
-        </section>
-        <section>
-          <h2>Contact Us</h2>
-          <p>You can reach us at example@email.com</p>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; {new Date().getFullYear()} My React App</p>
-      </footer> */}
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import '../UI/Home.css';
+import Button from './Button';
 
 function Home() {
-  const backgroundImageUrl = '/background.jpg';
+  const linkedinUrl = '/linkedin.png';
 
   return (
-    <div className="home-container" style={{
-      backgroundImage: `url(${backgroundImageUrl})`,
-      backgroundSize: 'cover',
-      backgroundSize: '100% 100%',
-      minHeight: '100vh',
-    }}>
+    <div className="home-container">
     <header className="header">
-      <p className="p"> LIVIA BONIFÁCIO </p>
-      <a href="/contact" className="contact-link">CONTACT</a> {/* Add the contact link */}
-      <a href="/projects" className="contact-link">PROJECTS</a> {/* Add the contact link */}
-      <a href="/about" className="contact-link">ABOUT</a> {/* Add the contact link */}
-      <a href="/home" className="contact-link">HOME</a> {/* Add the contact link */}
+      <p className="p"> PORTFOLIO </p>
+      <a href="/contact" className="contact-link">CONTACT</a>
+      <a href="/projects" className="contact-link">PROJECTS</a> 
+      <a href="/about" className="contact-link">ABOUT</a> 
+      <a href="/home" className="contact-link">HOME</a> 
     </header>
+      <div className='content'>
+        <h1 className="h1">Hi, my name is Livia Bonifácio</h1>
+        <h4 className="h4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
+            <Button>PROJECTS</Button>
+       </div>
+
+           {/* Links de Mídia Social */}
+        <div className="social-media-links">
+        <a href="https://www.linkedin.com/seu-usuario" target="_blank" rel="noopener noreferrer">
+          {/* <img src={linkedinUrl} alt="LinkedIn" />  */}
+        </a>
+        </div>
     </div>
   );
 }
