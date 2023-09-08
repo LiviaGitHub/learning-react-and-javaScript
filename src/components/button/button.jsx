0db8613c1d6f1ee2f3575ac72fd51f5a4ec682f0
LiviaGitHub <liviagithub@gmail.com>
2023-09-08 08:40:40 +0200

    changes after review

---
+++ src/components/button/button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.css';
+
+export const Button = (props) => {
+    return (
+        <button 
+            onClick={props.onClick} 
+            className="button"
+            type={props.type || 'button'} >
+            {props.children}
+        </button>
+    );
+};
+
+export default Button;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
-import '../UI/Home.css';
-import '../UI/SocialMedia.css';
-import stylesButton from './Button';
+import './home.css';
+import stylesButton from '../button/button.css'; 
+import Button from '../button/button.jsx';
 
 function Home() {
-  const linkedinUrl = '/linkedin.png';
 
   return (
     <div className="home-container">
       <header className="header">
-        <p className="p"> PORTFOLIO </p>
-        <a href="/contact" className="contact-link">CONTACT</a>
-        <a href="/projects" className="contact-link">PROJECTS</a> 
-        <a href="/about" className="contact-link">ABOUT</a> 
-        <a href="/home" className="contact-link">HOME</a> 
+        <p className="p"> portfolio </p>
+        <a href="/contact" className="contact-link">contact</a>
+        <a href="/projects" className="contact-link">projects</a> 
+        <a href="/about" className="contact-link">about</a> 
+        <a href="/home" className="contact-link">home</a> 
       </header>
       <div className='content'>
         <h1 className="h1">Hi, my name is Livia Bonifácio</h1>
         <h4 className="h4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
         <a className={stylesButton.a} href="/projects">
-          <Button>PROJECTS</Button> 
+          <Button>projects</Button> 
         </a>
       </div>
 
       {/* Links de Mídia Social */}
-      <div className="container">
+      <div className="social_media">
         <a href="https://www.linkedin.com/seu-usuario" target="_blank" rel="noopener noreferrer">
           {/* <img src={linkedinUrl} alt="LinkedIn" />  */}
         </a>
