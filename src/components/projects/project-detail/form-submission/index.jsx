7248f5196c4a8f8a279c125929bfeca25ad23b8e
LiviaGitHub <liviagithub@gmail.com>
2023-10-12 15:44:51 +0200

    more leranings

--- src/components/projects/project-detail/form-submission/index.jsx
+++ src/components/projects/project-detail/form-submission/index.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '../../../header';
 
 export const FormSubmissionPage = () => {
 
   const[enteredName, setEnteredName] = useState('');
+  const[enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const[enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => { 
+    if(enteredNameIsValid){
+      console.log('Name input is valid');
+    } 
+  }, [enteredNameIsValid]);
 
   const nameInputHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,17 +19,38 @@ export const FormSubmissionPage = () => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    setEnteredNameTouched(true);
+    
+    if(enteredName.trim() === ''){
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
+
+    setEnteredName('');
   };
 
+  const nameImputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameImputClasses = enteredNameIsValid ? 'form-control invalid' : 'form-control';
+
   return (
-    <div className='home-container'>
+    <div>
       <Header />
       <h1>Form submission and getting User imput values</h1>
       <form onSubmit={formSubmissionHandler}>
-        <div className='form-control'>
+        <div className={nameImputClasses}>
           <label htmlFor='name'>Name</label>
-          <input type='text' id='name' onChange={nameInputHandler} />
+          <input 
+            type='text' 
+            id='name' 
+            onChange={nameInputHandler}  
+            value={enteredName}/>
+            {nameImputIsInvalid && <p> Name must not be empty. </p>}
           <button> Submit </button>
         </div>
       </form>
