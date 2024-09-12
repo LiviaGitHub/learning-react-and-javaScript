5703acbbbc0d908d374c7111ccc1832a0326b447
Livia Bonifacio <50983740+LiviaGitHub@users.noreply.github.com>
2024-09-12 06:37:57 +0200

    chnage-props

--- src/components/projects/project-detail/all-about-props/images.js
+++ src/components/projects/project-detail/all-about-props/images.js
@@ -1,5 +1,5 @@
 export const images = {
-    katsukoSaruhashi: 'https://i.imgur.com/YfeOqp2.jpg',
+    katsukoSaruhashi: 'https://i.imgur.com/OKS67lh.jpg',
     akliluLemma: 'https://i.imgur.com/OKS67lh.jpg',
     linLanying: 'https://i.imgur.com/1bX5QH6.jpg',
   };
--- src/components/projects/project-detail/all-about-props/index.jsx
+++ src/components/projects/project-detail/all-about-props/index.jsx
@@ -13,48 +13,95 @@ export function Card({ children }) {
 
 export function Avatar({ person, size = 100 }) {
     return (
-        <img
-            className="avatar"
-            src={getImageUrls(person)}
-            alt={person.name}
-            width={size}
-            height={size}
-        />
+        <div>
+            <img
+                className="avatar circle-img"
+                src={getImageUrls(person)}
+                alt={person.name}
+                width={size}
+                height={size}
+            />
+            {/* class 'name' */}
+            {/* JSX expressions must have one parent element. */}
+            <h2 className="name">{person.name}</h2>
+        </div>
     );
 }
+
+
+export function AvatarTrad(props) {
+    return (
+      <img
+        className="avatar circle-img"
+        src={getImageUrls(props.person)}
+        alt={props.person.name}
+        width={props.size || 100}
+        height={props.size || 100}
+      />
+    );
+}
+
+export function Info(props) {
+    return (
+        <div className="info">
+            <h2 className="name"> {props.name} </h2>
+            <p> {props.info} </p>
+        </div>
+    );
+}
+
 export const AllAboutPropsPage = () => {
     return (
         <div className='home-container'>
             <Header />
             <h1>All about Props</h1>
+
             <Card>
                 <Avatar
                     size={100}
                     person={{
-                        name: 'Katsuko Saruhashi',
-                        imageId: 'YfeOqp2'
+                        name: 'Aklilu Lemma',
+                        imageId: 'akliluLemma'
                     }}
                 />
             </Card>
-            <Avatar size={80} person={{ name: 'Aklilu Lemma', imageId: 'akliluLemma' }} />
-            <Avatar person={{ name: 'Lin Lanying', imageId: 'linLanying' }} />
+
+            <Card>
+                <Avatar 
+                    size={80} 
+                    person={{ 
+                        name: 'Aklilu Lemma', 
+                        imageId: 'akliluLemma' 
+                    }} 
+                />
+            </Card>
+
+            <Info
+                name="Livia"
+                info="Software developer"
+            />
+
+            <Avatar 
+                person={{ 
+                    name: 'Lin Lanying', 
+                    imageId: 'linLanying' 
+                }} 
+            />
+
+            <Info
+                name="Alice"
+                info="Doctor"
+            />
+
+            <Card>
+                <AvatarTrad
+                    size={80} 
+                    person={{ 
+                        name: 'Aklilu Lemma', 
+                        imageId: 'akliluLemma' 
+                    }} 
+                />
+            </Card>
         </div >
     );
 };
-
-/**
- * 
- * Props is data coming from the outside, and can only be updated by the parent component.
- * Props are immutable.
- * If you need to mutate props you actually need stated.
- * Props has one-way DATA flow
- * <Avatar {...props} /> This forwards all of AllAboutPropsPage to the Avatar without listing each of their names.
- * To pass props, add them to the JSX, just like you would with HTML attributes.
- * To read props, use the function Avatar({ person, size }) destructuring syntax.
- * You can specify a default value like size = 100, which is used for missing and undefined props.
- * You can forward all props with <Avatar {...props} /> JSX spread syntax, but don’t overuse it!
- * Nested JSX like <Card><Avatar /></Card> will appear as Card component’s children prop.
- * Props are read-only snapshots in time: every render receives a new version of props.
- * You can’t change props. When you need interactivity, you’ll need to set state.
- * 
- */
