74e4ccf13d9b300e01550bbe0c2fe66567905528
Livia Bonifacio <50983740+LiviaGitHub@users.noreply.github.com>
2024-10-01 08:52:18 +0200

    ObjectAndArrayDestructuringPage

---
+++ src/components/projects/project-detail/object-array-destructuring/data.js
@@ -0,0 +1,25 @@
+const cars = [
+  {
+    model: 'Honda Civic',
+    coloursByPopularity: ['black', 'silver'],
+    speedStats: {
+      topSpeed: 140,
+      zeroToSixty: 8.5
+    }
+  },
+  {
+    model: 'Tesla Model 3',
+    coloursByPopularity: ['red', 'white'],
+    speedStats: {
+      topSpeed: 150,
+      zeroToSixty: 3.2
+    }
+  }
+];
+
+export default cars;
+
+  // First car object
+  // The first color in the array below is the most popular
+  // property in the code is an array that lists car colors
+  // Nested object with two properties: topSpeed and zeroToSixty
--- src/components/projects/project-detail/object-array-destructuring/index.jsx
+++ src/components/projects/project-detail/object-array-destructuring/index.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
-// import './index.css';
+import cars from './data';
+
 import { Header } from '../../../header';
 
 export const ObjectAndArrayDestructuringPage = () => {
-    return (
-        <div>
-            <Header />
-            <div>
 
-            </div>
-        </div>
-    );
-}
+    const [honda, tesla] = cars;
+
+    const {
+      speedStats: { topSpeed: hondaTopSpeed }
+    } = honda;
+    const {
+      speedStats: { topSpeed: teslaTopSpeed }
+    } = tesla;
+    
+    const {
+      coloursByPopularity: [hondaTopColour]
+    } = honda;
+    const {
+      coloursByPopularity: [teslaTopColour]
+    } = tesla;
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <table>
+          <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+            <th>Top Colour</th>
+          </tr>
+          <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+          </tr>
+          <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+// Destructuring is being used to simplify code by allowing you to quickly access properties of objects and arrays 
+// within honda and tesla without having to write long accessor chains like 
+// honda.speedStats.topSpeed or tesla.coloursByPopularity[0].
+
+// This makes the code cleaner, easier to read, and less error-prone 
+// because you don't have to repeat the accessor structure.
