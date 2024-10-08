42fac2271b6297c6e4afc479ac782a8aa2b531e8
Livia Bonifacio <50983740+LiviaGitHub@users.noreply.github.com>
2024-10-08 06:12:57 +0200

    form event

--- src/components/projects/project-detail/form-event/index.jsx
+++ src/components/projects/project-detail/form-event/index.jsx
@@ -3,43 +3,58 @@ import './index.css';
 import { Header } from '../../../header';
 
 export const FormEventPage = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  // const [headingText, setHeadingText] = useState('Livia');
 
-    const [country, setCountry] = useState('');
-    const [headingText, setHeadingText] = useState('Brazil');
+  function handleChangeFirstName(event) {
+    setName(event.target.value);
+  }
 
-    function handleChange(event) {
-        setCountry(event.target.value)
-    }
+  function handleChangeLastName(event) {
+    setLastName(event.target.value);
+  }
 
-    function handleClick() {
-        setHeadingText(country)
+  // function handleChange(event) {
+  //     setName(event.target.value)
+  // }
 
-        event.preventDefault();
-    }
+  // function handleClick() {
+  //     setHeadingText(name)
 
-    return (
-        <div>
-            <Header />
-            <form className='form-container'>
-                <h1 className='form-h1'>
-                    My country is {headingText}
-                </h1>
-                <input
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="What's your name?"
-                    value={country}
-                />
-                <button
-                type='Submit'
-                    onClick={handleClick}
-                >
-                    Send
-                </button>
-            </form>
-        </div>
-    );
-}
+  //     event.preventDefault();
+  // }
+
+  return (
+    <div>
+      <Header />
+      <form className="form-container">
+        <h1 className="form-h1">
+          Hello {name} {lastName}
+        </h1>
+        <input
+          onChange={handleChangeFirstName}
+          type="text"
+          placeholder="First name"
+          value={name}
+        />
+        <input
+          onChange={handleChangeLastName}
+          // onChange={handleChange}
+          type="text"
+          placeholder="Last name"
+          value={lastName}
+        />
+        <button
+          type="Submit"
+          // onClick={handleClick}
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
 // https://legacy.reactjs.org/docs/forms.html#controlled-components
 // event is the change event.
